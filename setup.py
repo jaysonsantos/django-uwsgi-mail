a9bbbd6d755e7ba0283e37cafc9eb5b1747721d9
Jayson Reis <santosdosreis@gmail.com>
2011-10-11 18:20:48 -0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+    name='django-uwsgi-mail',
+    version='1.0',
+    author='Jayson Reis',
+    author_email='santosdosreis@gmail.com',
+    description='A Django backend for e-mail delivery using uWSGI Spool to queue deliveries.',
+    url='https://github.com/jaysonsantos/django-uwsgi-mail',
+    packages=['uwsgi_mail'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+    ]
+)
