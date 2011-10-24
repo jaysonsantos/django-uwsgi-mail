f6d0ee6fb2724f951802723954a98041a6c5f444
Jayson Reis <santosdosreis@gmail.com>
2011-10-24 11:27:33 -0200

    fixed tests

--- test_project/views.py
+++ test_project/views.py
@@ -2,7 +2,6 @@ from django.core import mail
 from django.http import HttpResponse
 
 def test(request):
-    print dir(mail)
     mail.send_mail('Subject here', 'Here is the message.', 'from@example.com',
               ['to@example.com'], fail_silently=False)
     return HttpResponse('Queued')
--- test_project/wsgi.py
+++ test_project/wsgi.py
@@ -3,7 +3,7 @@ import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
     '..')))
-sys.path.append(os.path.join(os.path.dirname(__file__))
+sys.path.append(os.path.join(os.path.dirname(__file__)))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'test_project.settings'
 import django.core.handlers.wsgi
 
