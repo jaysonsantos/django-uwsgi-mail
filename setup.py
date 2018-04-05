31266ae1f68d2439def778cdfb59360f79daeed8
Jayson Reis <santosdosreis@gmail.com>
2018-04-05 12:10:27 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='django-uwsgi-mail',
-    version='1.1.1',
+    version='1.2.0',
     author='Jayson Reis',
     author_email='santosdosreis@gmail.com',
     description='A Django backend for e-mail delivery using uWSGI Spool to queue deliveries.',
@@ -11,6 +11,8 @@ setup(
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
     ],
     install_requires=['uwsgidecorators==1.1.0']
 )
